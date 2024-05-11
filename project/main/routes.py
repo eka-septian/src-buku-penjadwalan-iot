e7ba5c8ed4ba0e426cc95b14869b896578fa120f
Octa Toriq <Octa.Toriq@gmail.com>
2024-05-12 02:26:09 +0700

    update baseLayout,authStyle
    todo:unittest

--- project/main/routes.py
+++ project/main/routes.py
@@ -1,12 +1,18 @@
 from flask import render_template
+from flask_login import login_required
 
 from . import bp
 
 
 @bp.route("/")
+@login_required
 def home():
     return render_template("home.html")
 
+@bp.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
 
 @bp.route('/fungsi')
 def pushbtn_state():
